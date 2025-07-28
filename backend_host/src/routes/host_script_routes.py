e98563cccce81592922a0871cc4750d902aa0681
AngelStreet <ndoye.joachim@gmail.com>
2025-07-28 21:55:03 +0200

    polling script

--- backend_host/src/routes/host_script_routes.py
+++ backend_host/src/routes/host_script_routes.py
@@ -2,7 +2,7 @@
 Host Script Routes - Execute scripts on host device
 """
 from flask import Blueprint, request, jsonify
-from utils.script_utils import execute_script
+from shared.lib.utils.script_utils import execute_script
 
 host_script_bp = Blueprint('host_script', __name__, url_prefix='/host')
 
