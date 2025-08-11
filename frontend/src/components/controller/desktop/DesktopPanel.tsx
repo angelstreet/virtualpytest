9a257d6fc2e62dc2753012b6fda189d1165ba6bb
AngelStreet <ndoye.joachim@gmail.com>
2025-08-11 23:09:35 +0200

    reduce

--- frontend/src/components/controller/desktop/DesktopPanel.tsx
+++ frontend/src/components/controller/desktop/DesktopPanel.tsx
@@ -46,10 +46,10 @@ export const DesktopPanel = React.memo(function DesktopPanel({
   const [pyAutoGUICollapsed, setPyAutoGUICollapsed] = useState(false);
 
   // Updated panel dimensions for split layout with dynamic space allocation
-  const collapsedWidth = '420px';
-  const collapsedHeight = '450px';
-  const expandedWidth = '520px';
-  const expandedHeight = '650px';
+  const collapsedWidth = '350px';
+  const collapsedHeight = '240px';
+  const expandedWidth = '500px';
+  const expandedHeight = '360px';
   const headerHeight = '48px';
   const sectionHeaderHeight = '32px';
 
